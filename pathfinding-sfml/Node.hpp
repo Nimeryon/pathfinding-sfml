a7104c84ab36c8cde768ce9ff15dbb2ed4e894ec
Hugo Pradel <hugo.pradel@actulog.fr>
2023-12-11 16:12:52 +0100

    Add list dans utils à templater et les h du Astar et Node

---
+++ pathfinding-sfml/Node.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+struct Node
+{
+	Node(unsigned m_x, unsigned m_y) : x(m_x), y(m_y) {}
+
+	struct
+	{
+		unsigned x, y;
+
+		int cost_f = 0;
+		int cost_g = 0;
+		int cost_h = 0;
+
+		Node* parent = nullptr;
+
+	};
+
+	bool operator == (const Node& n) const
+	{
+		return x == n.x && y == n.y;
+	}
+
+	bool operator != (const Node& n) const
+	{
+		return x != n.x || y != n.y;
+	}
+};
