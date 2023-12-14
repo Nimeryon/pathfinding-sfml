21610aa257e8decca27352931bae5fafd725fe35
Thomas <t.auriol46@gmail.com>
2023-12-14 22:18:46 +0100

    Update Main.cpp

--- pathfinding-sfml/Main.cpp
+++ pathfinding-sfml/Main.cpp
@@ -6,11 +6,6 @@
 
 int main()
 {
-    // Exemple
-    Grid<Tile> grid(10, 10);
-    grid.GetValue(0, 0).SetTileType(TileType::START);
-    std::cout << grid;
-
     Game game("Default Scene");
 
     return EXIT_SUCCESS;
